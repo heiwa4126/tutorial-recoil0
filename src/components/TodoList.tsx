c69d49c582a342c240e1d1c0074c5bac4c298c1c
heiwa4126 <heiwa4126@gmail.com>
2022-12-13 14:35:20 +0900

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,47 @@
-import { useRecoilValue } from "recoil";
+import { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { todoListState } from "../atom";
 import TodoListStats from "./TodoListStats";
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  const [title, setTitle] = useState("");
+  //const todoList = useRecoilValue(todoListState);
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        title,
+        isComplete: false,
+      },
+    ]);
+    setTitle("");
+  };
 
   console.log(todoList);
   return (
     <>
       <h1>RecoilによるTodoアプリ</h1>
       <TodoListStats />
+      <div style={{ margin: "1em 0" }}>
+        <input type="text" value={title} onChange={handleChange} />
+        <button onClick={addItem}>Add</button>
+      </div>
       {todoList.map((item) => (
         <div key={item.id}>{item.title}</div>
       ))}
     </>
   );
+}
+
+let id = 1;
+function getId() {
+  return id++;
 }
 
 export default TodoList;
