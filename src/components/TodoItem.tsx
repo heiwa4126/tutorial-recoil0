230b7cf514e6be46d8ccfe6bf68e2785b305c8da
heiwa4126 <heiwa4126@gmail.com>
2022-12-13 14:48:47 +0900

    update

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,32 @@
 import Todo from "../types/Todo";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../atom";
 
 type Prop = {
   item: Todo;
 };
 
 function TodoItem({ item }: Prop) {
-  return <div>{item.title}</div>;
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
+  const deleteItem = () => {
+    console.log("delete", item.id);
+    const index = todoList.findIndex((listItem) => listItem.id === item.id);
+    const newTodoList = [
+      ...todoList.slice(0, index),
+      ...todoList.slice(index + 1),
+    ];
+    setTodoList(newTodoList);
+  };
+
+  return (
+    <div>
+      {item.title}
+      <span onClick={deleteItem} style={{ cursor: "pointer" }}>
+        X
+      </span>
+    </div>
+  );
 }
 
 export default TodoItem;
