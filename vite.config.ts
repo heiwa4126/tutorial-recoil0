32b06a84e01169afdd34f2acf8171ab53dcc825d
heiwa4126 <heiwa4126@gmail.com>
2022-12-13 19:19:04 +0900

    add terser options

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        passes: 2,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     rollupOptions: {
       output: {
         manualChunks: {
