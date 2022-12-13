8b93a2c981d3f7f1c7eb42996a0c4d2ed7ce09cb
heiwa4126 <heiwa4126@gmail.com>
2022-12-13 14:38:26 +0900

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,36 +2,17 @@ import { useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { todoListState } from "../atom";
 import TodoListStats from "./TodoListStats";
+import TodoItemCreator from "./TodoItemCreator";
 
 function TodoList() {
-  const [title, setTitle] = useState("");
-  //const todoList = useRecoilValue(todoListState);
-  const [todoList, setTodoList] = useRecoilState(todoListState);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
-  };
-
-  const addItem = () => {
-    setTodoList((oldTodoList) => [
-      ...oldTodoList,
-      {
-        id: getId(),
-        title,
-        isComplete: false,
-      },
-    ]);
-    setTitle("");
-  };
+  const todoList = useRecoilValue(todoListState);
 
   console.log(todoList);
   return (
     <>
       <h1>RecoilによるTodoアプリ</h1>
       <TodoListStats />
-      <div style={{ margin: "1em 0" }}>
-        <input type="text" value={title} onChange={handleChange} />
-        <button onClick={addItem}>Add</button>
-      </div>
+      <TodoItemCreator />
       {todoList.map((item) => (
         <div key={item.id}>{item.title}</div>
       ))}
