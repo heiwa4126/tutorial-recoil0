2aac8ae2d8e7bc867f8d2c1c5ed4399fc48bad26
heiwa4126 <heiwa4126@gmail.com>
2022-12-13 14:53:05 +0900

    update

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -19,8 +19,21 @@ function TodoItem({ item }: Prop) {
     setTodoList(newTodoList);
   };
 
+  const toggleItemCompletion = () => {
+    const index = todoList.findIndex((listItem) => listItem.id === item.id);
+    const newTodoList = [
+      ...todoList.slice(0, index),
+      { ...item, isComplete: !item.isComplete },
+      ...todoList.slice(index + 1),
+    ];
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
+      <button onClick={toggleItemCompletion}>
+        {item.isComplete ? "完" : "未"}
+      </button>
       {item.title}
       <span onClick={deleteItem} style={{ cursor: "pointer" }}>
         X
