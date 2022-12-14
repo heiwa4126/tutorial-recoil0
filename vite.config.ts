21b0f7a505b8c778403920bc31218eebf44881c0
heiwa4126 <heiwa4126@gmail.com>
2022-12-14 09:30:56 +0900

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        passes: 2,
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          v: ["react", "react-dom", "recoil", "immer"],
+export default defineConfig(({ command }: ConfigEnv) => {
+  return {
+    plugins: [react()],
+    esbuild:
+      command === "build"
+        ? {
+            drop: ["console", "debugger"], // https://esbuild.github.io/api/#drop
+          }
+        : null,
+    build: {
+      //// https://terser.org/docs/api-reference#minify-options
+      // minify: "terser",
+      // terserOptions: {
+      //   compress: {
+      //     passes: 2,
+      //     drop_console: true,
+      //     drop_debugger: true,
+      //   },
+      // },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            v: ["react", "react-dom", "recoil", "immer"],
+          },
         },
       },
     },
-  },
+  };
 });
